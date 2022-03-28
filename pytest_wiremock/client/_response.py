a9c918d0a378368a2b9e038d7f7ba03c4a1f98fb
symonk <jackofspaces@gmail.com>
2022-03-28 16:41:26 +0100

    adding a delegate response object to wrap `httpx.Response`

---
+++ pytest_wiremock/client/_response.py
@@ -0,0 +1,20 @@
+import httpx
+
+
+class WiremockResponse:
+    """
+    The response object for all calls to wiremock.
+    """
+    def __init__(self, delegate: httpx.Response) -> None:
+        self.delegate = delegate
+
+    def __getattr__(self, item: str):
+        """ Dynamic dispatch to the underlying response. """
+        attr = getattr(self.delegate, item)
+        if callable(item):
+            def wrapper(*args, **kwargs):
+                return attr(*args, **kwargs)
+            return wrapper
+        return attr
+
+
